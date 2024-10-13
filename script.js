35258a0962d70fdf6b46251600fbb0e1069c5101
ikiww?maybe <gamaugelay524@gmail.com>
2024-10-14 01:51:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,17 +12,22 @@ function myMenuFunction() {
 
 // dark mode
 
-const body = document.querySelector("body"),
-    toggleSwitch = document.getElementById("toggle-switch");
+const body = document.querySelector("body");
+const toggleSwitch = document.getElementById("toggle-switch");
+const typingElements = document.getElementsByClassName("typedText");
 
 toggleSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
+    // Loop melalui semua elemen yang memiliki kelas "typedText"
+    for (let i = 0; i < typingElements.length; i++) {
+        typingElements[i].classList.toggle("dark");
+    }
 });
 
 // typing effect
 
 var typingEffect = new Typed(".typedText", {
-    strings: ["Designer", "Coder", "Developer"],
+    strings: ["Designer", "Progaming", "Developer"],
 
     loop: true,
     typeSpeed: 100,
@@ -46,6 +51,16 @@ sr.reveal(".text-btn", { delay: 200 });
 sr.reveal(".social_icons", { delay: 200 });
 sr.reveal(".featured-image", { delay: 320 });
 
+ScrollReveal().reveal('.project-card', {
+    duration: 1700,
+    distance: '50px',
+    opacity: 0,
+    origin: 'left',
+    interval: 200,
+    reset: true,
+});
+
+
 
 sr.reveal(".project-box", { setInterval: 200 });
 
@@ -61,6 +76,7 @@ const srLeft = ScrollReveal({
 
 srLeft.reveal(".about-info", { delay: 100 });
 srLeft.reveal(".contact-info", { delay: 100 });
+srLeft.reveal(".text-area", { delay: 230 });
 
 const srRight = ScrollReveal({
     origin: "left",
@@ -71,6 +87,7 @@ const srRight = ScrollReveal({
 
 srRight.reveal(".skill", { delay: 100 });
 srRight.reveal(".skill-box", { delay: 100 });
+srRight.reveal(".input-field", { delay: 350 });
 
 
 // active link
@@ -101,4 +118,15 @@ function scollActive() {
 
 window.addEventListener("scroll", scollActive)
 
+ // Initialize the map
+ var map = L.map('map').setView([-6.1751, 106.8650], 13); 
 
+ // Set up the OpenStreetMap layer
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: ''
+ }).addTo(map);
+
+ // Add a marker to the map
+ var marker = L.marker([-6.1751, 106.8650]).addTo(map)
+     .bindPopup('<b>My Location</b><br>This is where I am based.')
+     .openPopup();
